86c77071fe0c1a4d259e268d45323b06f06e197c
谢孔营 <xiekongying001@ke.com>
2019-04-25 20:35:01 +0800

    上传项目

---
+++ performance-tools/src/main/java/com/performance/tools/loader/PerformaceClient.java
@@ -0,0 +1,10 @@
+package com.performance.tools.loader;
+
+/**
+ * 创建时间: 2019/04/25 10:48 <br>
+ * 作者: xiekongying001 <br>
+ * 描述:
+ */
+public class PerformaceClient {
+
+}
---
+++ performance-tools/src/main/java/com/performance/tools/parser/base/IInfo.java
@@ -0,0 +1,17 @@
+package com.performance.tools.parser.base;
+
+import android.content.ContentValues;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * 创建时间: 2019/04/25 11:58 <br>
+ * 作者: xiekongying001 <br>
+ * 描述:
+ */
+public interface IInfo {
+  JSONObject toJson() throws JSONException;
+  void parserJson(JSONObject json) throws JSONException;
+  ContentValues toContentValues();
+  String getId();
+}
